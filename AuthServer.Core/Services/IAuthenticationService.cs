af4f73ba6fbe64c71220493fd1b49dcd16a284a2
Kamran <311.kamran@gmail.com>
2023-08-30 14:22:04 +0400

    IAuthenticataionService interface'inin yaradilmasi

---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using SharedLibrary.Dtos;
+using AuthServer.Core.Dtos;
+
+namespace AuthServer.Core.Services;
+
+public interface IAuthenticationService
+{
+	Task<Response<TokenDto>> CreateToken(LogInDto logIn);
+	Task<Response<TokenDto>> CreateTokenByRefleshToken(string refleshToken);
+	Task<Response<NoDataDto>> RevokeRefleshToken(string refleshToken);
+	Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLogInDto clientLogInDto);
+}
