26e2a61e1233a23081ac32481630ab806c4a7fca
Kamran <311.kamran@gmail.com>
2023-08-30 16:18:28 +0400

    AutoMapper library'sinin elave edilmesi
    DtoMapper ve ObjectMapper classlarinin yaradilmasi

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+
+namespace AuthServer.Service;
+
+class DtoMapper : Profile
+{
+	public DtoMapper()
+	{
+		CreateMap<ProductDto, Product>().ReverseMap();
+		CreateMap<UserAppDto, UserApp>().ReverseMap();
+	}
+}
