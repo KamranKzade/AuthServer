a01a6eb24d3587071bdb898bc13af343ec10401d
Kamran <311.kamran@gmail.com>
2023-08-30 15:39:01 +0400

    UnitOfWork class'inin yaradilmasi

--- AuthServer.Data/Repositories/GenericRepository.cs
+++ AuthServer.Data/Repositories/GenericRepository.cs
@@ -1,11 +1,6 @@
-﻿using AuthServer.Core.Repositories;
+﻿using System.Linq.Expressions;
+using AuthServer.Core.Repositories;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AuthServer.Data.Repositories
 {
