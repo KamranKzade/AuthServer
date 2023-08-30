23d83a018ce686c2a2dd13c52849d6452c676382
Kamran <311.kamran@gmail.com>
2023-08-30 14:13:31 +0400

    ITokenService interface'inin yaradilmasi ve Client classinin yaradilmasi

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration;
+
+public class Client
+{
+    public string Id { get; set; }
+    public string Secret { get; set; }
+
+    // Hansi Api -a muraciet ede biler, onu yaziriq burda
+    public List<string> Audiences { get; set; }
+}
