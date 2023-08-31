c88fa2346c01cb95f38d35c7d5d720f5a55aad37
Kamran <311.kamran@gmail.com>
2023-08-31 17:07:09 +0400

    Product Controller in yaradilmasi ve kodlanmasi

---
+++ AuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthServer.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : CustomBaseController
+{
+	private readonly IServiceGeneric<Product, ProductDto> _productService;
+
+	public ProductController(IServiceGeneric<Product, ProductDto> serviceGeneric)
+	{
+		_productService = serviceGeneric;
+	}
+
+
+	[HttpGet]
+	public async Task<IActionResult> GetProduct()
+	{
+		return ActionResultInstance(await _productService.GetAllAsync());
+	}
+
+
+	[HttpPost]
+	public async Task<IActionResult> SaveProduct(ProductDto product)
+	{
+		return ActionResultInstance(await _productService.AddAsync(product));
+	}
+
+
+	[HttpPut]
+	public async Task<IActionResult> UpdateProduct(ProductDto product)
+	{
+		return ActionResultInstance(await _productService.UpdateAsync(product, product.Id));
+	}
+
+
+	[HttpDelete]
+	public async Task<IActionResult> DeleteProduct(int id)
+	{
+		return ActionResultInstance(await _productService.RemoveAsync(id));
+	}
+}
