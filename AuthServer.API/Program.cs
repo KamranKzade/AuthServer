9743b32e859aa344a25e77f976941bdca64e0932
Kamran <311.kamran@gmail.com>
2023-08-31 12:06:19 +0400

    Program.cs file-nin kodlanmasi
    AddScope, AddIdentity, AddDbContext elave edilmesi

--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -1,5 +1,14 @@
 using SharedLibrary.Configuration;
 using AuthServer.Core.Configuration;
+using AuthServer.Core.Services;
+using AuthServer.Service.Services;
+using AuthServer.Core.Repositories;
+using AuthServer.Data.Repositories;
+using AuthServer.Core.UnitOfWork;
+using AuthServer.Data;
+using Microsoft.EntityFrameworkCore;
+using AuthServer.Core.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +18,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DI lari sisteme tanitmaq
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IServiceGeneric<,>), typeof(ServiceGeneric<,>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Connectioni veririk
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), sqlOptions =>
+	{
+		// Migration-i harada etmek isteyirikse onun assemblesini yaziriq 
+		sqlOptions.MigrationsAssembly("AuthServer.Data");
+	});
+});
+
+// Identity-ni sisteme tanidiriq
+builder.Services.AddIdentity<UserApp, IdentityRole>(opt =>
+{
+	// Userin, Password uzerinde deyisiklikler edirik
+	opt.User.RequireUniqueEmail = true;
+	opt.Password.RequireNonAlphanumeric = false;
+}).AddEntityFrameworkStores<AppDbContext>()
+  .AddDefaultTokenProviders();
+
 
 // TokenOption elave edirik Configure-a 
 // Option pattern --> DI uzerinden appsetting-deki datalari elde etmeye deyilir.
