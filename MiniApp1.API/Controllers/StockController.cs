e6bc10f21515e316918892135b97464941a115eb
Kamran <311.kamran@gmail.com>
2023-09-01 10:24:43 +0400

    Mini API'larimiz ucun controller yaratmaq

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MiniApp1.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class StockController : ControllerBase
+{
+	public IActionResult GetStock()
+	{
+		var userName = HttpContext.User.Identity.Name;
+		var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+
+		// db-dan userId veya username uzerinden lazimli datalari cek
+		return Ok($"Stock islemleri ==> Username: {userName}-- UserId:{userId}");
+	}
+
+}
---
+++ MiniApp2.API/Controllers/InvoiceController.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MiniApp2.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class InvoiceController : ControllerBase
+{
+	public IActionResult GetInvoice()
+	{
+		var userName = HttpContext.User.Identity.Name;
+		var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+
+		// db-dan userId veya username uzerinden lazimli datalari cek
+		return Ok($"Invoice islemleri ==> Username: {userName}-- UserId:{userId}");
+	}
+
+}
