ffa38caf93723bce226ee0f7346799d2b70c2c07
Kamran <311.kamran@gmail.com>
2023-09-01 11:11:20 +0400

    Fluent Validation library'sinin projeye daxil etmek

--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -11,13 +11,19 @@ using AuthServer.Core.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using SharedLibrary.Extentions;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
+									
+builder.Services.AddControllers()
+				.AddFluentValidation(opts => // FluentValidationlari sisteme tanidiriq
+{
+	opts.RegisterValidatorsFromAssemblyContaining<Program>();
+});
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
